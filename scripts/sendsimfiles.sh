f9f7e445732f26f468ca1aa1298cf2760080a14b
Ian Klatzco <iklatzco@gmail.com>
2018-08-27 16:33:36 -0500

    improve sendsimfiles

--- scripts/sendsimfiles.sh
+++ scripts/sendsimfiles.sh
@@ -9,21 +9,21 @@ ITGMACHINEIP="5" # change this if the machine's not responding: it means that th
 function sendsimfiles() {
 
 	if [ -z "$folder" ] # if $folder is not set
-		printf "\nPlease set $folder in .bashrc.\n"
-		exit 1
+	then
+		printf "\nPlease export \$folder in .bashrc.\n\n"
+		return 1
 	fi
 
-	printf "\nMake sure you're on the right wifi network.\n"
+	printf "\nMake sure you're on the right wifi network.\n\n"
 
 	cd $localpath
-
 	# verify we're inside their song folder.
 	currentdir=${PWD##*/}
 	if [ "$currentdir" == "$folder" ]; then
-		rsync -vru --delete ./ wendy@192.168.1.$ITGMACHINEIP:"/home/wendy/songs/$folder" || failed
+		rsync -vru --delete ./ wendy@192.168.1.$ITGMACHINEIP:"/home/wendy/songs/$folder" || return 1
 	else
 		printf "\nYour folder doesn't exist or isn't in the right place.\n"
-		failed
+		return 1
 	fi
 
 	printf "\nSimfiles successfully transferred.\n"
@@ -31,10 +31,5 @@ function sendsimfiles() {
 
 function reloadsongs() {
 	ssh wendy@192.168.1.$ITGMACHINEIP -t 'exec /home/wendy/util/reload-songs.sh'
-}
-
-function failed() {
-	echo "something's wrong. contact ian or andrew."
-	exit 1
 }
 
